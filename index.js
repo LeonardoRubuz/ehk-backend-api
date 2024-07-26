cb6907b8de4e0db7fe5e47bf46408ee58145a77c
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-07-26 12:31:07 +0100

    for prod

--- index.js
+++ index.js
@@ -54,6 +54,6 @@ server.use("/users", userRouter)
 
 
 
-server.listen(port, host,  () => {
-    console.log(`Server listening on http://${host}:${port}`);
+server.listen(port, () => {
+    console.log(`Server listening on http://${port}`);
 })
